b67d6c901144538418d211a66c6540e32be18f1c
InputYourName <liveintt@gmail.com>
2018-05-16 22:52:14 +0800

    希尔排序

--- src/main/java/com/zhi/algorithm/App.java
+++ src/main/java/com/zhi/algorithm/App.java
@@ -11,6 +11,12 @@ public class App {
     for (int i : a) {
       System.out.printf("%d ", i);
     }
+    System.out.println();
+    int[] b = {2, 7, 1, 9, 4, 2, 5, 8, 2, 5, 3};
+    shell_sort(b);
+    for (int i : b) {
+      System.out.printf("%d ", i);
+    }
   }
 
   /**
@@ -27,6 +33,23 @@ public class App {
         a[j] = a[j - 1];//数组元素后移
       }
       a[j] = temp;
+    }
+  }
+
+  /** 希尔排序，一种分组排序算法
+   * @param a
+   */
+  private static void shell_sort(int[] a) {
+    int n = a.length;
+    for (int i = n / 2; i > 0; i /= 2) {
+      for (int j = i; j < n; j++) {
+        int temp = a[j];
+        int k = j;
+        for (; k >= i && a[k - i] > temp; k -= i) {
+          a[k] = a[k - i];
+        }
+        a[k] = temp;
+      }
     }
   }
 }
