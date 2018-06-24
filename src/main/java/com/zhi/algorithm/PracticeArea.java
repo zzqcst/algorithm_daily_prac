851fb9e2309afe492d1a1d2bfdf6ecc77acc935d
zhi <zzqlrr@gmail.com>
2018-06-24 23:13:38 +0800

    leetcode prac

--- src/main/java/com/zhi/algorithm/PracticeArea.java
+++ src/main/java/com/zhi/algorithm/PracticeArea.java
@@ -4,48 +4,96 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class PracticeArea {
     public static void main(String[] args) {
-//        javaOpenDoc();
-//        bigDecimalDemo();
-//
-//        float a =3;
-//        float b = 2.4f;
-//        System.out.println(a-b);
-//        String temp = "";
-//        List<String> list = new ArrayList<>();
-//        list.add("hello");
-//        System.out.println((temp=list.get(0)).equals("hello")?temp:"no");
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        int len=removeDuplicates(nums);
-        System.out.println();
-        for (int i=0;i<len;i++) {
-            System.out.print(nums[i]+" ");
-        }
     }
 
-    public static int removeDuplicates(int[] nums) {
-        int i=0;
-        for (int j = 1; j <nums.length ; j++) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i]=nums[j];
+    public static boolean containsDuplicate(int[] nums) {
+        return true;
+    }
+
+
+    /**
+     * 旋转数组
+     * @param nums
+     * @param k
+     */
+    private static void rotate(int[] nums, int k) {
+        int[] re = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int pos = (i + k) % nums.length;
+            re[pos]=nums[i];
+        }
+        System.arraycopy(re,0,nums,0,re.length);
+
+        //O(1) method
+        /*
+              public void rotate(int[] nums, int k) {
+                  int n = nums.length;
+                  k = k%n;
+                  swap(nums,0,n-k-1);
+                  swap(nums,n-k,n-1);
+                  swap(nums,0,n-1);
+              }
+              private void swap(int[] nums, int i, int j) {
+                  while (i < j) {
+                      int temp = nums[i];
+                      nums[i] = nums[j];
+                      nums[j] = temp;
+                      i++;
+                      j--;
+                  }
+              }
+         */
+    }
+
+
+    /**
+     * 股票最大收益
+     * @param prices
+     * @return
+     */
+    private static int maxProfit(int[] prices) {
+        int max = 0;
+        for (int i=1; i < prices.length; i++) {
+            int diff = prices[i] - prices[i - 1];
+            if (diff > 0) {
+                max += diff;
             }
         }
-        return i+1;
+        return max;
     }
 
+
+    /**
+     * 删除重复值
+     * @param nums
+     * @return
+     */
+    private static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    /**
+     * BigDecimal使用
+     */
     private static void bigDecimalDemo() {
         BigDecimal b1 = new BigDecimal("3.0");
         BigDecimal b2 = new BigDecimal("2.4");
         System.out.println(b1.subtract(b2));
     }
 
+    /**
+     * 打开本地word文档
+     */
     private static void javaOpenDoc() {
         try {
             Desktop.getDesktop().open(new File("F:\\IdeaProjects\\algorithm_daily_prac\\src\\main\\java\\com\\zhi\\algorithm\\a.docx"));
@@ -54,17 +102,4 @@ public class PracticeArea {
         }
     }
 
-    public static String replaceSpace(StringBuffer str) {
-        char[] dest = new char[str.length()];
-        str.getChars(0,str.length(),dest,0);
-        StringBuilder result= new StringBuilder();
-        for (int i = 0; i < dest.length; i++) {
-            if (dest[i] != ' ') {
-                result.append(dest[i]);
-            }else {
-                result.append("%20");
-            }
-        }
-        return result.toString();
-    }
 }
