b219190ced2da97e7d5cdd7e199e352bf7e51d17
InputYourName <liveintt@gmail.com>
2018-07-02 22:10:42 +0800

    sync

--- src/main/java/com/zhi/algorithm/PracticeArea.java
+++ src/main/java/com/zhi/algorithm/PracticeArea.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 public class PracticeArea {
     public static void main(String[] args) {
-        ListNode node1 = new ListNode(1);
-//        ListNode node2 = new ListNode(2);
-//        node1.next=node2;
-        removeNthFromEnd(node1, 1);
+        StringBuffer sb = new StringBuffer();
+        sb.append("hello world;");
+        sb.replace(sb.lastIndexOf(";"), sb.lastIndexOf(";")+1, "");
+        System.out.println(sb.toString());
     }
 
     /**
