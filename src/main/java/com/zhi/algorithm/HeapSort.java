4de4c3a52acdf546f15152855a54061c2c1a8052
zhi <zzqlrr@gmail.com>
2018-12-07 19:23:56 +0800

    sync

--- src/main/java/com/zhi/algorithm/HeapSort.java
+++ src/main/java/com/zhi/algorithm/HeapSort.java
@@ -8,13 +8,14 @@ import java.util.Arrays;
  */
 public class HeapSort {
     public static void main(String []args){
-        int []arr = {9,8,7,6,5,4,3,2,1};
+        int []arr = {0,1,2,3,4,5,6,7,8,9};
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
+
     private static void sort(int[] arr){
         //1.构建大顶堆
-        for(int i=arr.length/2-1;i>=0;i--){
+        for(int i=arr.length/2-1;i>=0;i--){//第一个非叶子节点的下标是len/2-1,完全二叉树的性质
             //从第一个非叶子结点从下至上，从右至左调整结构
             adjustHeap(arr,i,arr.length);
         }
@@ -27,20 +28,20 @@ public class HeapSort {
     }
 
     /**
-     * 调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+     * 调整大顶堆
      * @param arr
      * @param i
      * @param length
      */
     private static void adjustHeap(int[] arr, int i, int length){
         int temp = arr[i];//先取出当前元素i
-        for(int k=i*2+1;k<length;k=k*2+1){//从i结点的左子结点开始，也就是2i+1处开始
+         for(int k=i*2+1;k<length;k=k*2+1){//从i结点的左子结点开始，也就是2i+1处开始，最后的k=k*2+1是本层比较完之后，再下降一层到子节点继续调整
             if(k+1<length && arr[k]<arr[k+1]){//如果左子结点小于右子结点，k指向右子结点
                 k++;
             }
             if(arr[k] >temp){//如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
                 arr[i] = arr[k];
-                i = k;
+                i = k;//i变为子节点的索引
             }else{
                 break;
             }
