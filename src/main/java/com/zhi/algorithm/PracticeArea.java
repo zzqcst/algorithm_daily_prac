2417335662af2d9c3b0456fb62f3fe2201336058
InputYourName <liveintt@gmail.com>
2019-01-18 20:51:09 +0800

    sync

--- src/main/java/com/zhi/algorithm/PracticeArea.java
+++ src/main/java/com/zhi/algorithm/PracticeArea.java
@@ -138,6 +138,26 @@ public class PracticeArea {
 
     }
 
+    /**
+     *
+     给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+
+     例如，给出 n = 3，生成结果为：
+
+     [
+     "((()))",
+     "(()())",
+     "(())()",
+     "()(())",
+     "()()()"
+     ]
+     * @param n
+     * @return
+     */
+    public static List<String> generateParenthesis(int n) {
+        return null;
+    }
+
     private static String[] maps = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
     /**
