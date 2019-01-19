e084d3743edb047e890ef969d93e4ed62a3781c5
zhi <zzqcst@gmail.com>
2019-01-19 14:40:22 +0800

    sync

--- src/main/java/com/zhi/algorithm/PracticeArea.java
+++ src/main/java/com/zhi/algorithm/PracticeArea.java
@@ -140,9 +140,45 @@ public class PracticeArea {
         //            System.out.println(string);
         //        }
         int[] nums = { 1, 2, 3 };
-        List<List<Integer>> permute = permute(nums);
+        List<List<Integer>> permute = subsets(nums);
         for (List<Integer> integers : permute) {
             System.out.println(integers.toString());
+        }
+    }
+
+    /**
+     * 子集
+     * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+     * 说明：解集不能包含重复的子集。
+     * 示例:
+     * 输入: nums = [1,2,3]
+     * 输出:
+     * [
+     * [3],
+     * [1],
+     * [2],
+     * [1,2,3],
+     * [1,3],
+     * [2,3],
+     * [1,2],
+     * []
+     * ]
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        getsubset(nums,0, new ArrayList<Integer>(), res);
+        return res;
+    }
+
+    private static void getsubset(int[] nums,int from, ArrayList<Integer> level, List<List<Integer>> res) {
+        for (int i = from; i < nums.length ; i++) {
+            ArrayList<Integer> list = new ArrayList<>(level);
+            list.add(nums[i]);
+            res.add(list);
+            getsubset(nums, i + 1, list, res);
         }
     }
 
