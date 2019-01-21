d71d60d38830589982aff65239388df83d4b9a5f
zhi <zzqcst@gmail.com>
2019-01-21 15:18:14 +0800

    sync

--- src/main/java/com/zhi/algorithm/PracticeArea.java
+++ src/main/java/com/zhi/algorithm/PracticeArea.java
@@ -154,6 +154,43 @@ public class PracticeArea {
     }
 
     /**
+     * 在排序数组中查找元素的第一个和最后一个位置
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     * <p>
+     * 你的算法时间复杂度必须是 O(log n) 级别。
+     * <p>
+     * 如果数组中不存在目标值，返回 [-1, -1]。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: nums = [5,7,7,8,8,10], target = 8
+     * 输出: [3,4]
+     * 示例 2:
+     * <p>
+     * 输入: nums = [5,7,7,8,8,10], target = 6
+     * 输出: [-1,-1]
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = {-1, -1};
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target && res[0] == -1) {//res[0]==-1表示第一次出现
+                res[0] = i;
+                res[1] = i;
+                continue;
+            }
+            if (nums[i] == target) {
+                res[1] = i;
+            }
+
+        }
+        return res;
+    }
+
+    /**
      * 寻找峰值
      * 峰值元素是指其值大于左右相邻值的元素。
      * <p>
