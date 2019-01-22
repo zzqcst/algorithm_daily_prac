49a34e70dea4a6123ad9c84da7d801c60d6d4167
zhi <zzqcst@gmail.com>
2019-01-22 16:09:53 +0800

    sync

--- src/main/java/com/zhi/algorithm/PracticeArea.java
+++ src/main/java/com/zhi/algorithm/PracticeArea.java
@@ -181,8 +181,61 @@ public class PracticeArea {
 //        for (Interval interval4 : merge(list)) {
 //            System.out.println(interval4);
 //        }
-        int[] nums = {3, 2, 1, 0, 4};
-        System.out.println(canJump(nums));
+
+        System.out.println(uniquePaths(7, 3));
+    }
+
+    /**
+     * 不同路径
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+     * <p>
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+     * <p>
+     * 问总共有多少条不同的路径？
+     * <p>
+     * 说明：m 和 n 的值均不超过 100。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: m = 3, n = 2
+     * 输出: 3
+     * 解释:
+     * 从左上角开始，总共有 3 条路径可以到达右下角。
+     * 1. 向右 -> 向右 -> 向下
+     * 2. 向右 -> 向下 -> 向右
+     * 3. 向下 -> 向右 -> 向右
+     * 示例 2:
+     * <p>
+     * 输入: m = 7, n = 3
+     * 输出: 28
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public static int uniquePaths(int m, int n) {
+        //递归方法
+        int poss = 0;
+        Map<String, Integer> map = new HashMap<>();
+        return nextStep(1, 1, m, n, poss, map);
+    }
+
+    private static int nextStep(int right, int down, int m, int n, int poss, Map<String, Integer> map) {
+        if (map.containsKey(right + "," + down)) {
+            return map.get(right + "," + down);
+        }
+        if (right > n || down > m) {
+            return 0;
+        }
+        if (right == n && down == m) {
+            poss++;
+            return poss;
+        }
+        int stepr = nextStep(right + 1, down, m, n, poss, map);
+        map.put((right + 1) + "," + down, stepr);
+        int stepd = nextStep(right, down + 1, m, n, poss, map);
+        map.put(right + "," + (down + 1), stepd);
+        return stepr + stepd;
     }
 
     /**
