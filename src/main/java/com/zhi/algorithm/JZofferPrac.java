0dddebb5903356ea1031376e11f037c367f006c6
zhi <zzqcst@gmail.com>
2019-02-21 14:36:12 +0800

    sync

--- src/main/java/com/zhi/algorithm/JZofferPrac.java
+++ src/main/java/com/zhi/algorithm/JZofferPrac.java
@@ -19,9 +19,172 @@ public class JZofferPrac {
 
     public static void main(String[] args) {
         JZofferPrac p = new JZofferPrac();
-        int[] pre = {1, 2, 3, 4, 5, 6, 7};
-        int[] in = {3, 2, 4, 1, 6, 5, 7};
-        p.reConstructBinaryTree(pre, in);
+        System.out.println(p.NumberOf1(1));
+    }
+
+    /**
+     * 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+     *
+     * @param n
+     * @return
+     */
+    public int NumberOf1(int n) {
+        //有多少个1就计算多少次的解法
+        int count = 0;
+        while (n != 0) {
+            //一个数减一之后与原来的数按位与操作，相当于把最右边的1变成0，
+            // 那么原数的二进制有多少个1，就会运算多少次
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+        //原数有多少位就计算多少次的方法
+        //将1与原数按位相与，每次计算之后，1左移一位
+//        int c = 0;
+//        int flag = 1;
+//        while (flag != 0) {
+//            if ((n & flag) != 0) {
+//                c++;
+//            }
+//            flag = flag << 1;
+//        }
+//        return c;
+    }
+
+    /**
+     * 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+     * 请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+     *
+     * @param target
+     * @return
+     */
+    public int RectCover(int target) {
+        //本质还是斐波那契，前两个元素为1、2
+        if (target == 1) {
+            return 1;
+        }
+        if (target == 2) {
+            return 2;
+        }
+        int prepre = 1;
+        int pre = 2;
+        int cur = 0;
+        for (int i = 3; i < target; i++) {
+            cur = pre + prepre;
+            prepre = pre;
+            pre = cur;
+        }
+        return cur;
+    }
+
+    /**
+     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
+     * 求该青蛙跳上一个n级的台阶总共有多少种跳法
+     *
+     * @param target
+     * @return
+     */
+    public int JumpFloorII(int target) {
+        //        if(target==1){
+        //            return 1;
+        //        }
+        //         return 2<<(target-2);
+        return (int) Math.pow(2, target - 1);
+    }
+
+    /**
+     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。
+     * 求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+     *
+     * @param target
+     * @return
+     */
+    public int JumpFloor(int target) {
+        if (target == 1) {
+            return 1;
+        }
+        if (target == 2) {
+            return 2;
+        }
+        int preisone = 2;
+        int preistwo = 1;
+        int cur = 0;
+        for (int i = 3; i <= target; i++) {
+            cur = preisone + preistwo;
+            preistwo = preisone;
+            preisone = cur;
+        }
+        return cur;
+    }
+
+    /**
+     * 大家都知道斐波那契数列，现在要求输入一个整数n，
+     * 请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+     * n<=39
+     *
+     * @param n
+     * @return
+     */
+    public int Fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int prepre = 0;
+        int pre = 1;
+        int cur = 0;
+        for (int i = 2; i <= n; i++) {
+            cur = pre + prepre;
+            prepre = pre;
+            pre = cur;
+        }
+        return cur;
+    }
+
+    /**
+     * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+     * 输入一个非减排序的数组的一个旋转，输出旋转数组的最小元素。
+     * 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
+     * NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+     *
+     * @param array
+     * @return
+     */
+    public int minNumberInRotateArray(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+        int index1 = 0;
+        int index2 = array.length - 1;
+        int mid = 0;
+        while (array[index1] >= array[index2]) {
+            if (index2 - index1 == 1) {
+                mid = index2;
+                break;
+            }
+            mid = (index1 + index2) / 2;
+            if (array[index1] == array[index2] && array[index1] == array[mid]) {
+                return regufind(array, index1, index2);
+            }
+            if (array[index1] <= array[mid]) {
+                index1 = mid;
+            } else if (array[index2] >= array[mid]) {
+                index2 = mid;
+            }
+        }
+        return array[mid];
+    }
+
+    private int regufind(int[] array, int index1, int index2) {
+        int res = array[index1];
+        for (int i = index1 + 1; i <= index2; i++) {
+            if (res > array[i]) {
+                res = array[i];
+            }
+        }
+        return res;
     }
 
     //用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
