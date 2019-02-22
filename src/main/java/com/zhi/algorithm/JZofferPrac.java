4fe2a5ce668c23ec7837a1dd1a4c781e05d50e6a
InputYourName <liveintt@gmail.com>
2019-02-22 23:27:19 +0800

    sync

--- src/main/java/com/zhi/algorithm/JZofferPrac.java
+++ src/main/java/com/zhi/algorithm/JZofferPrac.java
@@ -31,17 +31,21 @@ public class JZofferPrac {
 
     public static void main(String[] args) {
         JZofferPrac p = new JZofferPrac();
-        TreeNode t1 = new TreeNode(1);
-//        TreeNode t2 = new TreeNode(2);
-//        TreeNode t3 = new TreeNode(3);
-//        TreeNode t4 = new TreeNode(4);
-//        TreeNode t5 = new TreeNode(4);
-//        TreeNode t6 = new TreeNode(6);
-//        t1.left = t2;
-//        t1.right = t3;
-//        t2.left = t4;
-//        t2.right = t5;
-//        t3.left = t6;
+        TreeNode t1 = new TreeNode(10);
+        TreeNode t2 = new TreeNode(6);
+        TreeNode t3 = new TreeNode(4);
+        TreeNode t4 = new TreeNode(8);
+        TreeNode t5 = new TreeNode(14);
+        TreeNode t6 = new TreeNode(16);
+        TreeNode t7 = new TreeNode(12);
+        t1.left = t2;
+        t1.right = t5;
+        t2.left = t3;
+        t2.right = t4;
+        t5.left = t7;
+        t5.right = t6;
+        p.Convert(t1);
+
 //        ArrayList<ArrayList<Integer>> lists = p.FindPath(t1, 7);
 //        for (ArrayList<Integer> list : lists) {
 //            for (Integer integer : list) {
@@ -50,6 +54,40 @@ public class JZofferPrac {
 //            System.out.println();
 //        }
 
+    }
+
+    /**
+     * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。
+     * 要求不能创建任何新的结点，只能调整树中结点指针的指向。
+     *
+     * @param pRootOfTree
+     * @return
+     */
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        //通过中序遍历得到升序的节点队列
+        if (pRootOfTree != null) {
+            TreeNode head = null;
+            Queue<TreeNode> queue = new LinkedList<>();
+            getNodes(pRootOfTree, queue);
+            head = queue.poll();
+            TreeNode temp = head;
+            while (!queue.isEmpty()) {//将队列中的节点重新连接
+                TreeNode curr = queue.poll();
+                temp.right = curr;
+                curr.left = temp;
+                temp = temp.right;
+            }
+            return head;
+        }
+        return null;
+    }
+
+    private void getNodes(TreeNode pRootOfTree, Queue<TreeNode> queue) {
+        if (pRootOfTree != null) {
+            getNodes(pRootOfTree.left, queue);
+            queue.add(pRootOfTree);
+            getNodes(pRootOfTree.right, queue);
+        }
     }
 
     /**
