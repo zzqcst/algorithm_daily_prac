6a71be6317758067a34174c009fc937e62e19f5d
InputYourName <liveintt@gmail.com>
2019-02-26 00:35:02 +0800

    sync

--- src/main/java/com/zhi/algorithm/JZofferPrac.java
+++ src/main/java/com/zhi/algorithm/JZofferPrac.java
@@ -31,19 +31,19 @@ public class JZofferPrac {
 
     public static void main(String[] args) {
         JZofferPrac p = new JZofferPrac();
-//        TreeNode t1 = new TreeNode(10);
-//        TreeNode t2 = new TreeNode(6);
-//        TreeNode t3 = new TreeNode(4);
-//        TreeNode t4 = new TreeNode(8);
-//        TreeNode t5 = new TreeNode(14);
-//        TreeNode t6 = new TreeNode(16);
-//        TreeNode t7 = new TreeNode(12);
-//        t1.left = t2;
-//        t1.right = t5;
-//        t2.left = t3;
-//        t2.right = t4;
-//        t5.left = t7;
-//        t5.right = t6;
+        TreeNode t1 = new TreeNode(8);
+        TreeNode t2 = new TreeNode(6);
+        TreeNode t3 = new TreeNode(6);
+        TreeNode t4 = new TreeNode(5);
+        TreeNode t5 = new TreeNode(7);
+        TreeNode t6 = new TreeNode(7);
+        TreeNode t7 = new TreeNode(5);
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = t4;
+        t2.right = t5;
+        t3.left = t6;
+        t3.right = t7;
 //        p.Convert(t1);
 
 //        ArrayList<ArrayList<Integer>> lists = p.FindPath(t1, 7);
@@ -53,8 +53,31 @@ public class JZofferPrac {
 //            }
 //            System.out.println();
 //        }
-        System.out.println(p.isNumeric("12e".toCharArray()));
+        System.out.println(p.isSymmetrical(t7));
 
+    }
+
+    /**
+     * 请实现一个函数，用来判断一颗二叉树是不是对称的。注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+     *
+     * @param pRoot
+     * @return
+     */
+    boolean isSymmetrical(TreeNode pRoot) {
+        return isSymmetrical(pRoot, pRoot);
+    }
+
+    private boolean isSymmetrical(TreeNode pRoot, TreeNode pRoot1) {
+        if (pRoot == null && pRoot1 == null) {
+            return true;
+        }
+        if (pRoot == null || pRoot1 == null) {
+            return false;
+        }
+        if (pRoot.val != pRoot1.val) {
+            return false;
+        }
+        return isSymmetrical(pRoot.left, pRoot1.right) && isSymmetrical(pRoot.right, pRoot1.left);
     }
 
     /**
