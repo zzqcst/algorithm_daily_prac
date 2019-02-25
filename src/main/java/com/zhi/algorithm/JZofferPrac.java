bcb4658814fa57c60cd9309dd5ce54ce557a9d33
InputYourName <liveintt@gmail.com>
2019-02-25 21:44:16 +0800

    sync

--- src/main/java/com/zhi/algorithm/JZofferPrac.java
+++ src/main/java/com/zhi/algorithm/JZofferPrac.java
@@ -53,8 +53,32 @@ public class JZofferPrac {
 //            }
 //            System.out.println();
 //        }
-        System.out.println(p.duplicate(new int[]{2, 3, 1, 0, 2, 5, 3}, 7, new int[1]));
+        System.out.println(p.multiply(new int[]{1, 2, 3, 4, 5}));
 
+    }
+
+    /**
+     * 给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],
+     * 其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法。
+     *
+     * @param A
+     * @return
+     */
+    public int[] multiply(int[] A) {
+        if (A.length <= 1) {
+            return null;
+        }
+        int[] B = new int[A.length];
+        B[0] = 1;
+        for (int i = 1; i < A.length; i++) {
+            B[i] = B[i - 1] * A[i - 1];
+        }
+        double temp = 1;
+        for (int i = A.length - 2; i >= 0; i--) {
+            temp = temp * A[i + 1];
+            B[i] = (int) (B[i] * temp);
+        }
+        return B;
     }
 
     /**
