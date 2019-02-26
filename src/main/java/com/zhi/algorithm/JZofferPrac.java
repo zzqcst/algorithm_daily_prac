1a655bc36c938ab4ffa85bd2ab670cf8e7e06315
InputYourName <liveintt@gmail.com>
2019-02-26 23:59:43 +0800

    sync

--- src/main/java/com/zhi/algorithm/JZofferPrac.java
+++ src/main/java/com/zhi/algorithm/JZofferPrac.java
@@ -87,6 +87,37 @@ public class JZofferPrac {
     }
 
     /**
+     * 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，
+     * 那么中位数就是所有数值排序之后位于中间的数值。
+     * 如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
+     * 我们使用Insert()方法读取数据流，使用GetMedian()方法获取当前读取数据的中位数。
+     *
+     * @param num
+     */
+    PriorityQueue<Double> queue3 = new PriorityQueue<>(new Comparator<Double>() {
+        @Override
+        public int compare(Double o1, Double o2) {
+            return (int) (o1 - o2);
+        }
+    });
+
+    public void Insert(Integer num) {
+        queue3.add(Double.valueOf(num));
+    }
+
+    public Double GetMedian() {
+        int size = queue3.size();
+        if (size % 2 != 0) {
+            int temp = size / 2 + 1;
+            while (temp != 0) {
+                queue3.poll();
+                temp--;
+            }
+        }
+        return 0.0;
+    }
+
+    /**
      * 给定一棵二叉搜索树，请找出其中的第k小的结点。例如， （5，3，7，2，4，6，8）    中，按结点数值大小顺序第三小结点的值为4。
      *
      * @param pRoot
