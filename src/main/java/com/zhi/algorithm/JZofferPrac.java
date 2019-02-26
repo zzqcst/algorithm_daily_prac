c46472ae7ba2ddcf17dbac1861e20e87d66ff89b
InputYourName <liveintt@gmail.com>
2019-02-26 23:31:22 +0800

    sync

--- src/main/java/com/zhi/algorithm/JZofferPrac.java
+++ src/main/java/com/zhi/algorithm/JZofferPrac.java
@@ -55,6 +55,7 @@ public class JZofferPrac {
         t2.right = t5;
         t3.left = t6;
         t3.right = t7;
+        System.out.println(p.Serialize(t1));
 //        p.Convert(t1);
 
 //        ArrayList<ArrayList<Integer>> lists = p.FindPath(t1, 7);
@@ -78,11 +79,51 @@ public class JZofferPrac {
 //        n5.next = n6;
 //        n6.next = n7;
 //        n1 = p.deleteDuplication(n1);
-        for (ArrayList<Integer> integers : p.Print(t1)) {
-            for (Integer integer : integers) {
-                System.out.print(integer + " ");
-            }
+//        for (ArrayList<Integer> integers : p.Print(t1)) {
+//            for (Integer integer : integers) {
+//                System.out.print(integer + " ");
+//            }
+//        }
+    }
+
+    String Serialize(TreeNode root) {
+        if (root == null)
+            return "";
+        StringBuilder sb = new StringBuilder();
+        Serialize2(root, sb);
+        return sb.toString();
+    }
+
+    void Serialize2(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("#,");
+            return;
         }
+        sb.append(root.val);
+        sb.append(',');
+        Serialize2(root.left, sb);
+        Serialize2(root.right, sb);
+    }
+
+    int index = -1;
+
+    TreeNode Deserialize(String str) {
+        if (str.length() == 0)
+            return null;
+        String[] strs = str.split(",");
+        return Deserialize2(strs);
+    }
+
+    TreeNode Deserialize2(String[] strs) {
+        index++;
+        if (!strs[index].equals("#")) {
+            TreeNode root = new TreeNode(0);
+            root.val = Integer.parseInt(strs[index]);
+            root.left = Deserialize2(strs);
+            root.right = Deserialize2(strs);
+            return root;
+        }
+        return null;
     }
 
     /**
