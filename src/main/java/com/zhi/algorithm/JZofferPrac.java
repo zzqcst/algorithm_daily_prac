d2c2f9df57952cb94158d496d0469b75132d2625
InputYourName <liveintt@gmail.com>
2019-02-26 23:50:42 +0800

    sync

--- src/main/java/com/zhi/algorithm/JZofferPrac.java
+++ src/main/java/com/zhi/algorithm/JZofferPrac.java
@@ -86,6 +86,31 @@ public class JZofferPrac {
 //        }
     }
 
+    /**
+     * 给定一棵二叉搜索树，请找出其中的第k小的结点。例如， （5，3，7，2，4，6，8）    中，按结点数值大小顺序第三小结点的值为4。
+     *
+     * @param pRoot
+     * @param k
+     * @return
+     */
+    int k;
+
+    TreeNode KthNode(TreeNode root, int k) {
+        this.k = k;
+        return helper(root);
+    }
+
+    public TreeNode helper(TreeNode root) {
+        TreeNode temp = null;
+        if (root != null) {
+            if ((temp = helper(root.left)) != null) return temp;
+            if (--k == 0) return root;
+            if ((temp = helper(root.right)) != null) return temp;
+
+        }
+        return null;
+    }
+
     String Serialize(TreeNode root) {
         if (root == null)
             return "";
