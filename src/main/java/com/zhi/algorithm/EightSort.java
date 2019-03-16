5c5f8d75a92b90ac23541e8879f90e8ca4ca54ce
InputYourName <liveintt@gmail.com>
2019-03-16 23:37:05 +0800

    sync

--- src/main/java/com/zhi/algorithm/EightSort.java
+++ src/main/java/com/zhi/algorithm/EightSort.java
@@ -6,7 +6,7 @@ package com.zhi.algorithm;
 public class EightSort {
     public static void main(String[] args) {
         int[] nums = {4, 9, 1, 3, 7, 9, 2, 5, 7, 3, 11, 19, 13, 14, 11, 16, 13};
-        shellOrder(nums, 5);
+        simpSelectSort(nums);
         for (int num : nums) {
             System.out.print(num + " ");
         }
@@ -34,21 +34,54 @@ public class EightSort {
         }
     }
 
-    public static void shellOrder(int[] arr, int dk) {
-        while (dk != 0) {
-            for (int i = 0; i < dk; i++) {
-                for (int j = i + dk; j < arr.length; j++) {
-                    if (arr[j] < arr[j - dk]) {
-                        int temp = arr[j];
-                        int h = 0;
-                        for (h = j - dk; h >= 0 && temp < arr[h]; h -= dk) {
-                            arr[h + dk] = arr[h];
-                        }
-                        arr[h + dk] = temp;
+    /**
+     * 希尔排序
+     * 先将整个待排序序列分割成若干子序列分别进行直接插入排序，待整个序列中的记录“基本有序时”,再对全体记录进行一次直接插入排序（增量为1）
+     * <p>
+     * 注意：增量序列中的值，除1之外，没有其它公因子，并且最后一个增量值必须等于1
+     * 时间复杂度与增量序列有关，使用Hibbard序列（Dk=2^k-1）,最坏情况O(N^3/2),平均O(N^5/4)
+     * 不稳定排序
+     *
+     * @param arr
+     */
+    public static void shellOrder(int[] arr) {
+        for (int dk = arr.length / 2; dk > 0; dk /= 2) {//希尔增量序列
+            for (int i = dk; i < arr.length; i++) {//插入排序
+                if (arr[i] < arr[i - dk]) {
+                    int temp = arr[i];
+                    int j = 0;
+                    arr[i] = arr[i - dk];
+                    for (j = i - dk - dk; j >= 0 && temp < arr[j]; j -= dk) {
+                        arr[j + dk] = arr[j];
                     }
+                    arr[j + dk] = temp;
                 }
             }
-            dk = dk / 2;
+        }
+
+    }
+
+    /**
+     * 简单选择排序
+     * 首先在未排序序列中找到最小（最大）元素，存放到排序序列的起始位置，然后每次从剩余未排序元素中继续寻找最小（最大）元素放到已排序序列末尾，以此类推。
+     * 最坏O(n^2) 最好O(n^2),平均O(n^2)
+     * 不稳定
+     *
+     * @param arr
+     */
+    public static void simpSelectSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;
+                }
+            }
+            if (index != i) {
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+            }
         }
     }
 }
