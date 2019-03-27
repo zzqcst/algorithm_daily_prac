03efe42295a602d6d2f36f6c814930861a64a27e
zhi <zzqcst@gmail.com>
2019-03-27 23:48:34 +0800

    sync

--- src/main/java/com/zhi/algorithm/PracticeArea.java
+++ src/main/java/com/zhi/algorithm/PracticeArea.java
@@ -187,8 +187,39 @@ public class PracticeArea {
 //            System.out.println(interval4);
 //        }
         PracticeArea p = new PracticeArea();
-        int[] a = {1, 2, 3, 5, 7};
-        System.out.println(p.numberOfArithmeticSlices(a));
+        int[][] grid = {{0, 1}, {1, 0}};
+        System.out.println(p.minPathSum(grid));
+    }
+
+    /**
+     * leetcode
+     * 最小路径和
+     * dp[i][j]表示到达(i,j)的最小代价
+     * 则dp[i][j]=min{dp[i-1][j]+grid[i][j],dp[i][j-1]+grid[i][j]}
+     *
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid) {
+        int rows = grid.length;
+        if (rows == 0) {
+            return 0;
+        }
+        int cols = grid[0].length;
+        int[][] dp = new int[rows][cols];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < cols; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[rows - 1][cols - 1];
     }
 
     /**
