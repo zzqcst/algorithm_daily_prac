35c510be44a3e5f69d67b4babe715b5dd0bda658
zhi <zzqcst@gmail.com>
2019-05-01 22:57:37 +0800

    sync

--- src/main/java/com/zhi/algorithm/PracticeArea.java
+++ src/main/java/com/zhi/algorithm/PracticeArea.java
@@ -244,52 +244,50 @@ public class PracticeArea {
         //
         //        }
         PracticeArea p = new PracticeArea();
-        for (Integer integer : p.splitIntoFibonacci("11235813")) {
-            System.out.print(integer+" ");
+        for (Integer integer : p.splitIntoFibonacci("0123")) {
+            System.out.print(integer + " ");
         }
     }
 
     /**
      * leetcode
      * 将数组拆分成斐波那契数列
+     * 回溯法
      *
      * @param S
      * @return
      */
     public List<Integer> splitIntoFibonacci(String S) {
-        /**
-         使用一个辅助List保存可行的序列, 在回溯过程中发现返回List不为空
-         说明有一种满足的序列产生提前终止回溯
-         **/
-        List<Integer> ret = new ArrayList<>();
-        bktrace(S, ret, new ArrayList<>(), 0);
-        return ret;
+        List<Integer> res = new ArrayList<>();
+        searchValidNums(S, res, new ArrayList<Integer>(), 0);
+        return res;
     }
 
-    private void bktrace(String s, List<Integer> ret, List<Integer> temp, int st) {
-        // 提前终止回溯
-        if(ret.size() != 0) return;
-        if(st == s.length() && temp.size() >= 3) {//字符串扫描结束
-            ret.addAll(temp);
+    private void searchValidNums(String s, List<Integer> res, ArrayList<Integer> list, int start) {
+        if (!res.isEmpty()) {
             return;
         }
-        for(int i = st; i < s.length(); ++i) {
-            // 每块数组除非为0否则不能以0开头
-            if(s.charAt(st) == '0' && i > st)
+        if (start == s.length()) {
+            if (list.size() >= 3) {
+                res.addAll(list);
+            }
+            return;
+        }
+        for (int i = start; i < s.length(); i++) {
+            if (s.charAt(start) == '0' && i > start) {
+                return;
+            }
+            long num = Long.valueOf(s.substring(start, i + 1));
+            if (num > Integer.MAX_VALUE) {
+                return;
+            }
+            if (list.size() >= 2 && (num > list.get(list.size() - 1) + list.get(list.size() - 2))) {
                 break;
-            // 每块数字不超过2^31-1
-            long num = Long.parseLong(s.substring(st, i+1));
-            if(num > Integer.MAX_VALUE)
-                break;
-
-            int size = temp.size();
-            // 提前终止回溯的条件, 注意是大于, 因为如果是小于的情况还可能继续添加num长度判断是否满足
-            if(size >= 2 && num > temp.get(size-1) + temp.get(size-2))
-                break;
-            else if(size < 2 || (num == temp.get(size-1) + temp.get(size-2))) {
-                temp.add((int)num);
-                bktrace(s, ret, temp, i+1);
-                temp.remove(temp.size()-1);
+            } else if (list.size() < 2 || (num == list.get(list.size() - 1) + list
+                    .get(list.size() - 2))) {
+                list.add((int) num);
+                searchValidNums(s, res, list, i + 1);
+                list.remove(list.size() - 1);
             }
         }
     }
