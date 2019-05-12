2664acd58d20edb6f6144b588cf2a76f0d90e356
InputYourName <liveintt@gmail.com>
2019-05-12 16:07:22 +0800

    sync

--- src/main/java/com/zhi/algorithm/Main.java
+++ src/main/java/com/zhi/algorithm/Main.java
@@ -16,7 +16,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+
+        }
     }
 
 
