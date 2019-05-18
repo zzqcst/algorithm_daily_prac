f59cff1db7d9c7e4fb501a7151411d938575a1c3
InputYourName <liveintt@gmail.com>
2019-05-18 22:45:43 +0800

    sync

--- src/main/java/com/zhi/algorithm/PracticeArea.java
+++ src/main/java/com/zhi/algorithm/PracticeArea.java
@@ -245,7 +245,42 @@ public class PracticeArea {
         //
         //        }
         PracticeArea p = new PracticeArea();
-        System.out.println(p.isValid2("aaabcbcbc"));
+        int[] a = {9, 3, 15, 20, 7};
+        int[] b = {9, 15, 7, 20, 3};
+        TreeNode treeNode = p.buildTree2(a, b);
+        List<Integer> list = inorderTraversal(treeNode);
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+
+    }
+
+    /**
+     * leetcode
+     * 从中序遍历与后序遍历序列构造二叉树
+     * <p>
+     * 后序遍历最后一个是根节点，中序遍历中，根节点左边的是左子树，右边的是右子树
+     *
+     * @param inorder
+     * @param postorder
+     * @return
+     */
+    public TreeNode buildTree2(int[] inorder, int[] postorder) {
+        return construct2(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+
+    private TreeNode construct2(int[] inorder, int infrom, int into, int[] postorder, int postfrom, int postto) {
+        if (infrom > into || postfrom > postto) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[postto]);
+        for (int i = infrom; i <= into; i++) {
+            if (inorder[i] == postorder[postto]) {
+                root.left = construct2(inorder, infrom, i - 1, postorder, postfrom, postfrom + i - infrom - 1);
+                root.right = construct2(inorder, i + 1, into, postorder, postfrom + i - infrom, postto - 1);
+            }
+        }
+        return root;
     }
 
     /**
