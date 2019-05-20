fefe070d42adde52d03008ac09d9488b4e0dd51a
InputYourName <liveintt@gmail.com>
2019-05-20 21:06:37 +0800

    sync

--- src/main/java/com/zhi/algorithm/JZofferPrac.java
+++ src/main/java/com/zhi/algorithm/JZofferPrac.java
@@ -42,24 +42,24 @@ public class JZofferPrac {
 
     public static void main(String[] args) {
         JZofferPrac p = new JZofferPrac();
-        TreeNode t1 = new TreeNode(8);
-        TreeNode t2 = new TreeNode(6);
-        TreeNode t3 = new TreeNode(3);
-        TreeNode t4 = new TreeNode(1);
-        TreeNode t5 = new TreeNode(7);
-        TreeNode t6 = new TreeNode(7);
-        TreeNode t7 = new TreeNode(5);
-        t1.left = t2;
-        t1.right = t3;
-        t2.left = t4;
-        t2.right = t5;
-        t3.left = t6;
-        t3.right = t7;
-        TreeNode convert = p.Convert(t1);
-        for (int i = 0; i < 7; i++) {
-            System.out.print(convert.val+" ");
-            convert=convert.right;
-        }
+//        TreeNode t1 = new TreeNode(8);
+//        TreeNode t2 = new TreeNode(6);
+//        TreeNode t3 = new TreeNode(3);
+//        TreeNode t4 = new TreeNode(1);
+//        TreeNode t5 = new TreeNode(7);
+//        TreeNode t6 = new TreeNode(7);
+//        TreeNode t7 = new TreeNode(5);
+//        t1.left = t2;
+//        t1.right = t3;
+//        t2.left = t4;
+//        t2.right = t5;
+//        t3.left = t6;
+//        t3.right = t7;
+//        TreeNode convert = p.Convert(t1);
+//        for (int i = 0; i < 7; i++) {
+//            System.out.print(convert.val+" ");
+//            convert=convert.right;
+//        }
 
 //        ArrayList<ArrayList<Integer>> lists = p.FindPath(t1, 7);
 //        for (ArrayList<Integer> list : lists) {
@@ -88,6 +88,7 @@ public class JZofferPrac {
 //            }
 //        }
 //        p.print1ToMaxOfNDigits(4);
+
     }
 
     /**
