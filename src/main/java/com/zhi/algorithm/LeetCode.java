886d5cfb5418b98c18f9f96dcf9cdc1074e48185
InputYourName <liveintt@gmail.com>
2019-05-28 10:13:56 +0800

    sync

--- src/main/java/com/zhi/algorithm/LeetCode.java
+++ src/main/java/com/zhi/algorithm/LeetCode.java
@@ -243,8 +243,100 @@ public class LeetCode {
         //
         //        }
         LeetCode p = new LeetCode();
-        System.out.println(p.poorPigs(25,15,60));
+        System.out.println(p.poorPigs(25, 15, 60));
 
+    }
+
+    /**
+     * LRU缓存机制
+     * <p>
+     * 使用双向链表，添加和查找时，将节点放到头部，移除时直接删除尾部节点
+     */
+    class LRUCache {
+        // 双链表节点
+        class Node {
+            int key;
+            int val;
+            Node prev;
+            Node next;
+        }
+
+        private int capacity;
+        private Node first;
+        private Node last;
+
+        private Map<Integer, Node> map;
+
+        public LRUCache(int capacity) {
+            this.capacity = capacity;
+            map = new HashMap<>(capacity);
+        }
+
+        public int get(int key) {
+            Node node = map.get(key);
+            if (node == null) {
+                return -1;
+            }
+            moveToHead(node);
+            return node.val;
+        }
+
+        private void moveToHead(Node node) {
+            if (node == first) {
+                return;
+            } else if (node == last) {
+                last.prev.next = null;
+                last = last.prev;
+            } else {
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+            }
+
+            node.prev = first.prev;
+            node.next = first;
+            first.prev = node;
+            first = node;
+        }
+
+        public void put(int key, int value) {
+            Node node = map.get(key);
+
+            if (node == null) {
+                node = new Node();
+                node.key = key;
+                node.val = value;
+
+                if (map.size() == capacity) {
+                    removeLast();
+                }
+
+                addToHead(node);
+                map.put(key, node);
+            } else {
+                node.val = value;
+                moveToHead(node);
+            }
+        }
+
+        private void addToHead(Node node) {
+            if (map.isEmpty()) {
+                first = node;
+                last = node;
+            } else {
+                node.next = first;
+                first.prev = node;
+                first = node;
+            }
+        }
+
+        private void removeLast() {
+            map.remove(last.key);
+            Node prevNode = last.prev;
+            if (prevNode != null) {
+                prevNode.next = null;
+                last = prevNode;
+            }
+        }
     }
 
     /**
@@ -256,8 +348,8 @@ public class LeetCode {
      * @return
      */
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int pigs=0;
-        int base=minutesToTest/minutesToDie+1;
+        int pigs = 0;
+        int base = minutesToTest / minutesToDie + 1;
         while (Math.pow(base, pigs) < buckets) {
             pigs++;
         }
