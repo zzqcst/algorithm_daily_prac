a430d4057aba96d05b178cf03f22677db85d22d2
InputYourName <liveintt@gmail.com>
2019-09-11 23:06:35 +0800

    sync

--- src/main/java/com/zhi/algorithm/Main.java
+++ src/main/java/com/zhi/algorithm/Main.java
@@ -2,10 +2,9 @@ package com.zhi.algorithm;
 
 import java.util.Scanner;
 
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
 }
