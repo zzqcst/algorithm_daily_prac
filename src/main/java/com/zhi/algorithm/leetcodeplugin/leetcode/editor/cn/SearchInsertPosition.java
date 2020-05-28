6f109a5482bd5da9bdf3247cc17e2d5722c82f25
zhi <zzqcst@gmail.com>
2020-05-29 00:47:00 +0800

    LeetCode Daily

---
+++ src/main/java/com/zhi/algorithm/leetcodeplugin/leetcode/editor/cn/SearchInsertPosition.java
@@ -0,0 +1,51 @@
+package com.zhi.algorithm.leetcodeplugin.leetcode.editor.cn;;
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。 
+//
+// 你可以假设数组中无重复元素。 
+//
+// 示例 1: 
+//
+// 输入: [1,3,5,6], 5
+//输出: 2
+// 
+//
+// 示例 2: 
+//
+// 输入: [1,3,5,6], 2
+//输出: 1
+// 
+//
+// 示例 3: 
+//
+// 输入: [1,3,5,6], 7
+//输出: 4
+// 
+//
+// 示例 4: 
+//
+// 输入: [1,3,5,6], 0
+//输出: 0
+// 
+// Related Topics 数组 二分查找
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums={1,3,8};
+        System.out.println(solution.searchInsert(nums, 9));
+    }
+}    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start =0,end=nums.length-1;
+        while (start<=end){ // 小于等于，注意
+            int mid = start+(end-start)/2;
+            if (target<nums[mid]) end=mid-1;
+            if (target>nums[mid]) start=mid+1;
+            if (target==nums[mid]) return mid;
+        }
+        return start;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
