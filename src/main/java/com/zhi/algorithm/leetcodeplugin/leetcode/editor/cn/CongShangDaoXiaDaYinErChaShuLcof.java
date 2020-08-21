22c3fbd2a3f9cbf63f7a7fa42f6dd7af0d14ff15
zhi <zzqcst@gmail.com>
2020-08-22 01:06:40 +0800

    LeetCode Daily

---
+++ src/main/java/com/zhi/algorithm/leetcodeplugin/leetcode/editor/cn/CongShangDaoXiaDaYinErChaShuLcof.java
@@ -0,0 +1,84 @@
+package com.zhi.algorithm.leetcodeplugin.leetcode.editor.cn;
+//从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。 
+//
+// 
+//
+// 例如: 
+//给定二叉树: [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回： 
+//
+// [3,9,20,15,7]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 1000 
+// 
+// Related Topics 树 广度优先搜索 
+// 👍 33 👎 0
+
+import com.zhi.algorithm.datastructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author zhi
+ */
+public class CongShangDaoXiaDaYinErChaShuLcof {
+    public static void main(String[] args) {
+        Solution solution = new CongShangDaoXiaDaYinErChaShuLcof().new Solution();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public int[] levelOrder(TreeNode root) {
+            if (root == null) return new int[0];
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            List<Integer> list = new ArrayList<>();
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode poll = queue.poll();
+                    list.add(poll.val);
+                    if (poll.left != null) {
+                        queue.offer(poll.left);
+                    }
+                    if (poll.right != null) {
+                        queue.offer(poll.right);
+                    }
+                }
+            }
+            int[] res = new int[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                res[i] = list.get(i);
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
