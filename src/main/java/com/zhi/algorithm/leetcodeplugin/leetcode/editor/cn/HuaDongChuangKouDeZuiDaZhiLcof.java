dee6adfb765bb6a056856993e5897b670a515348
zhuzhiqiang <zhuzhiqiang5@guazi.com>
2021-02-20 19:20:24 +0800

    lc daily

--- src/main/java/com/zhi/algorithm/leetcodeplugin/leetcode/editor/cn/HuaDongChuangKouDeZuiDaZhiLcof.java
+++ src/main/java/com/zhi/algorithm/leetcodeplugin/leetcode/editor/cn/HuaDongChuangKouDeZuiDaZhiLcof.java
@@ -26,15 +26,52 @@ package com.zhi.algorithm.leetcodeplugin.leetcode.editor.cn;
 // Related Topics 队列 Sliding Window 
 // 👍 187 👎 0
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class HuaDongChuangKouDeZuiDaZhiLcof {
     public static void main(String[] args) {
         Solution solution = new HuaDongChuangKouDeZuiDaZhiLcof().new Solution();
+        solution.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] maxSlidingWindow(int[] nums, int k) {
-            return null;
+            if (nums.length == 0 || k > nums.length) {
+                return new int[0];
+            }
+
+            int[] res = new int[nums.length - k + 1];
+            Map<Integer, Integer> map = new HashMap<>();
+            PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o2 - o1;
+                }
+            });
+            int i = 0, j = k - 1;
+            for (int l = i; l <= j; l++) {
+                queue.offer(nums[l]);
+            }
+            while (true) {
+                while (map.get(queue.peek()) != null && map.get(queue.peek()) != 0) {
+                    map.put(queue.peek(), map.get(queue.peek()) - 1);
+                    queue.poll();
+                }
+                res[i] = queue.peek();
+                j++;
+                if (j >= nums.length) {
+                    break;
+                }
+                queue.offer(nums[j]);
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+                i++;
+            }
+
+            return res;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
