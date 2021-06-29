518732dc286e665ad62dc242bababa84a996f9d8
zhi <zzqcst@gmail.com>
2021-06-29 23:25:12 +0800

    LeetCode Daily

---
+++ src/main/java/com/zhi/algorithm/leetcodeplugin/leetcode/editor/cn/SortList.java
@@ -0,0 +1,140 @@
+package com.zhi.algorithm.leetcodeplugin.leetcode.editor.cn;
+//给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。 
+//
+// 进阶： 
+//
+// 
+// 你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？ 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [4,2,1,3]
+//输出：[1,2,3,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [-1,5,3,4,0]
+//输出：[-1,0,3,4,5]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 5 * 104] 内 
+// -105 <= Node.val <= 105 
+// 
+// Related Topics 链表 双指针 分治 排序 归并排序 
+// 👍 1196 👎 0
+
+import com.zhi.algorithm.codes.LeetCode;
+import com.zhi.algorithm.datastructure.ListNode;
+
+/**
+ * @author zhi
+ */
+public class SortList {
+    public static void main(String[] args) {
+        Solution solution = new SortList().new Solution();
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+
+
+    class Solution {
+        //https://leetcode-cn.com/problems/sort-list/solution/pai-xu-lian-biao-by-leetcode-solution/
+        //自底向上归并排序，先拆成大小为1的链表，两两合并，大小为2的链表，两两合并。。。
+        public ListNode sortList(ListNode head) {
+            if (head == null) {
+                return head;
+            }
+            int length = 0;
+            ListNode node = head;
+            while (node != null) {
+                length++;
+                node = node.next;
+            }
+            ListNode dummyHead = new ListNode(0, head);
+            for (int subLength = 1; subLength < length; subLength <<= 1) {
+                ListNode prev = dummyHead, curr = dummyHead.next;
+                while (curr != null) {
+                    ListNode head1 = curr;
+                    for (int i = 1; i < subLength && curr.next != null; i++) {
+                        curr = curr.next;
+                    }
+                    ListNode head2 = curr.next;
+                    curr.next = null;
+                    curr = head2;
+                    for (int i = 1; i < subLength && curr != null && curr.next != null; i++) {
+                        curr = curr.next;
+                    }
+                    ListNode next = null;
+                    if (curr != null) {
+                        next = curr.next;
+                        curr.next = null;
+                    }
+                    ListNode merged = merge(head1, head2);
+                    prev.next = merged;
+                    while (prev.next != null) {
+                        prev = prev.next;
+                    }
+                    curr = next;
+                }
+            }
+            return dummyHead.next;
+        }
+
+        public ListNode merge(ListNode head1, ListNode head2) {
+            ListNode dummyHead = new ListNode(0);
+            ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+            while (temp1 != null && temp2 != null) {
+                if (temp1.val <= temp2.val) {
+                    temp.next = temp1;
+                    temp1 = temp1.next;
+                } else {
+                    temp.next = temp2;
+                    temp2 = temp2.next;
+                }
+                temp = temp.next;
+            }
+            if (temp1 != null) {
+                temp.next = temp1;
+            } else if (temp2 != null) {
+                temp.next = temp2;
+            }
+            return dummyHead.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
